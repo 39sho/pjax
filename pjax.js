a9778ee9cc90307a3ceaa7aecca13a517b1fc836
39sho <45312434+39sho@users.noreply.github.com>
2021-02-11 14:39:09 +0900

    Update pjax.js

--- pjax.js
+++ pjax.js
@@ -1,23 +1,21 @@
-const $a, $content;
-
 const init = () => {
-  $a = document.querySelector("a");
-  $content = document.querySelector("#content");
-};
+  const $a = document.querySelector("a");
+  const $content = document.querySelector("#content");
 
-$a.addEventListener("click", async (e) => {
-  e.preventDefault();
-  
-  console.log(e.target.href);
-  
-  const response = await fetch(e.target.href);
-  const text = await response.text();
-  
-  const html = new DOMParser().parseFromString(text, "text/html");
-  
-  $content.replaceWith(html.querySelector("#content"));
-  
-  init();
-});
+  $a.addEventListener("click", async (e) => {
+    e.preventDefault();
+    
+    console.log(e.target.href);
+    
+    const response = await fetch(e.target.href);
+    const text = await response.text();
+    
+    const html = new DOMParser().parseFromString(text, "text/html");
+    
+    $content.replaceWith(html.querySelector("#content"));
+    
+    init();
+  });
+};
 
 init();
