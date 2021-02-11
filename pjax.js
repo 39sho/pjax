2a1b89580a15bc67a0d41d8bbd3f6624b820c407
39sho <45312434+39sho@users.noreply.github.com>
2021-02-11 14:12:13 +0900

    Create pjax.js

---
+++ pjax.js
@@ -0,0 +1,15 @@
+const $a = document.querySelector("a");
+const $content = document.querySelector("#content");
+
+$a.addEventListener("click", async (e) => {
+  e.preventDefault();
+  
+  console.log(e.target.href;
+  
+  const response = await fetch(e.target.href);
+  const text = response.text();
+  
+  const html = new DOMParser().parseFromString(text, "text/html");
+  
+  $content.replaceWith(html.querySelector("#content"));
+});
